eac9e02b5af9c3052b716e88f1dc84369088772c
Mauricio <mauricionunesdasilvanunes@gmail.com>
2022-07-25 19:50:14 -0300

    add get client

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -3,12 +3,14 @@ const create = require('../controllers/clients/create')
 const index = require('../controllers/clients/index')
 const update = require('../controllers/clients/update')
 const destroy = require('../controllers/clients/destroy')
+const get = require('../controllers/clients/get')
 const router = express.Router()
 
 
 const auth = require('../middleware/auth')
 router.post('/', auth, create)
 router.get('/', auth, index)
+router.get('/search/:id', auth, get)
 router.put('/:id', auth, update)
 router.delete('/:id', auth, destroy)
 
